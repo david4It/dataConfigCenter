4ece8856abc78198fdc759716442c40ec8eb3ca4
david4It <david4it@163.com>
2020-08-03 15:55:23 +0800

    数据库结构更新

--- src/main/resources/db/base.sql
+++ src/main/resources/db/base.sql
@@ -9,9 +9,10 @@ CREATE TABLE `component`  (
   `layout_id` int(11) NOT NULL COMMENT '布局表的主键id',
   `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '标题',
   `type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '组件类型：line,bar,pie',
-  `query` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '数据查询语句',
+  `query` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '数据查询语句',
   `link` int(11) NULL DEFAULT NULL COMMENT '下钻对应的布局表主键id',
   `params` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '下钻传递的参数',
+  `category_value_pattern` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '图表键值所对应的字段，使用:进行分割，如key_field:value_field\r\n雷达存在多维度，故值的形式为:key_field:value_field1,value_field2,value_field3...',
   `width` int(10) NOT NULL COMMENT '宽度（比例）',
   `height` int(10) NOT NULL COMMENT '高度偏移量',
   `x` int(11) NOT NULL COMMENT 'x轴偏移（比例）',
@@ -19,9 +20,8 @@ CREATE TABLE `component`  (
   `location_index` int(10) NOT NULL COMMENT '位置索引：从左到右，从上到下，起始为0',
   `config_json` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '组件定制化配置（json数据格式）',
   `create_time` datetime(0) NOT NULL COMMENT '创建时间',
-  `last_update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 
 -- ----------------------------
